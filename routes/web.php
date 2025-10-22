6b456780a85ef55d9c9d6bef6e4cc4c52b5b013f
EL AKKAL AYOUB <akkalayoub3@gmail.com>
2025-10-22 18:23:44 +0000

    feat : create all quizz routes and refactor web.php

--- routes/web.php
+++ routes/web.php
@@ -2,14 +2,26 @@
 
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use App\Http\Controllers\QuizController;
 
-Route::get('/', function () {
-    return Inertia::render('Welcome');
-})->name('home');
+Route::redirect('/', '/dashboard');
 
-Route::get('dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::middleware('auth')->group(function () {
 
-require __DIR__.'/settings.php';
-require __DIR__.'/auth.php';
+    Route::get('dashboard', function () {
+        return Inertia::render('Dashboard');
+    })->name('dashboard');
+
+    Route::controller(QuizController::class)->name('quiz.')->group(function () {
+        Route::get('/quizzes', 'index')->name('index');
+        Route::get('quiz/{quiz}', 'show')->name('show');
+        Route::post('quiz', 'store')->name('store');
+    });
+});
+
+
+
+
+
+require __DIR__ . '/settings.php';
+require __DIR__ . '/auth.php';

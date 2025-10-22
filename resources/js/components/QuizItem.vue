80adae64660f1b6d0067902409da5e8266b9518b
EL AKKAL AYOUB <akkalayoub3@gmail.com>
2025-10-22 19:09:12 +0000

    feat : quizzes list

--- resources/js/components/QuizItem.vue
+++ resources/js/components/QuizItem.vue
@@ -1,20 +1,62 @@
 <script setup lang="ts">
-import { defineProps } from 'vue';
 import type { Quiz } from '@/types';
-import {  Link } from '@inertiajs/vue3';
+import { Link } from '@inertiajs/vue3';
+import { FilePenLine, Heart, Trash } from 'lucide-vue-next';
+import { defineProps } from 'vue';
+import Button from './ui/button/Button.vue';
 
-
-const props = defineProps<{
-  item: Quiz;
-}>();
-
+defineProps<{ item: Quiz }>();
 </script>
 <template>
     <div>
-        <h2>{{ item.title }}</h2>
-        <p>{{ item.description }}</p>
-        <!-- <Link :href="{ name: 'quizzes.show', params: { quiz: item.id } }">View Quiz</Link> -->
+        <div
+            class="w-full5 max-w-sm overflow-hidden rounded-lg bg-white shadow-md"
+        >
+            <div class="relative">
+                <img
+                    src="https://github.com/unovue.png"
+                    alt="Product image"
+                    class="h-64 w-full object-cover"
+                />
+                <button
+                    class="absolute top-2 right-2 rounded-full bg-white p-2 shadow-md transition-colors duration-200 hover:bg-gray-100"
+                >
+                    <Heart class="h-4 w-4" />
+                </button>
+            </div>
+            <div class="p-4">
+                <div class="mb-2 flex items-start justify-between">
+                    <div>
+                        <h2
+                            class="mb-1 line-clamp-1 text-xl font-semibold text-gray-800"
+                        >
+                            {{ item.title }}
+                        </h2>
+                    </div>
+                </div>
+                <div class="py-2">
+                    <p class="text-lg font-bold text-green-600">
+                        {{ item.status }}
+                    </p>
+                    <p class="text-sm text-gray-600">{{ item.expire_date }}</p>
+                </div>
+                <p class="mb-4 line-clamp-3 text-sm text-gray-600">
+                    {{ item.description }}
+                </p>
+                <div class="flex space-x-2">
+                    <Button
+                        :as="Link"
+                        size="sm"
+                        class="flex items-center justify-between gap-1.5 text-sm"
+                        :href="`/quiz/${item.id}`"
+                    >
+                        <FilePenLine class="h-4 w-4" /> Edit
+                    </Button>
+                    <Button variant="ghost" class="text-destructive" size="sm">
+                        <Trash class="h-4 w-4" /> Delete
+                    </Button>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
-
-

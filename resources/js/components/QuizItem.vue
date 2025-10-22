cc7bddbd57ab909f93a4b02fb6722792e0d6a1be
EL AKKAL AYOUB <akkalayoub3@gmail.com>
2025-10-22 18:29:31 +0000

    add quizzes page and initial quiz item for future display

---
+++ resources/js/components/QuizItem.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { defineProps } from 'vue';
+import type { Quiz } from '@/types';
+import {  Link } from '@inertiajs/vue3';
+
+
+const props = defineProps<{
+  item: Quiz;
+}>();
+
+</script>
+<template>
+    <div>
+        <h2>{{ item.title }}</h2>
+        <p>{{ item.description }}</p>
+        <!-- <Link :href="{ name: 'quizzes.show', params: { quiz: item.id } }">View Quiz</Link> -->
+    </div>
+</template>
+
+
---
+++ resources/js/pages/Quizzes/Index.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import QuizItem from '@/components/QuizItem.vue';
+import AppLayout from '@/layouts/AppLayout.vue';
+import { Quiz, type BreadcrumbItem } from '@/types';
+import { Head } from '@inertiajs/vue3';
+
+const props = defineProps<{
+    quizzes: Array<Quiz>;
+}>();
+
+const breadcrumbs: BreadcrumbItem[] = [
+    {
+        title: 'Quizzes',
+        href: '/quizzes',
+    },
+];
+</script>
+<template>
+    <Head title="Quizzes" />
+    <AppLayout :breadcrumbs="breadcrumbs">
+        <div class="grid w-full gap-4 p-5 md:grid-cols-2 lg:grid-cols-3">
+            <QuizItem v-for="quiz in quizzes" :key="quiz.id" :item="quiz" />
+        </div>
+    </AppLayout>
+</template>

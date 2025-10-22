dd81740d542069d16f5a462a8cfa96506981e7a7
EL AKKAL AYOUB <akkalayoub3@gmail.com>
2025-10-22 16:59:07 +0000

    add relationships between quiz and question

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -3,8 +3,21 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Question extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'type',
+        'question',
+        'description',
+        'data',
+        'quiz_id',
+    ];
+    public function quiz()
+    {
+        return $this->belongsTo(Quiz::class);
+    }
 }

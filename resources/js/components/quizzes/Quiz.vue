0d81b59984299f63f89407c27486c7168b810759
EL AKKAL AYOUB <akkalayoub3@gmail.com>
2025-10-29 19:12:25 +0000

    feat: refactor Quiz component to use computed properties for status and format date function

--- resources/js/components/quizzes/Quiz.vue
+++ resources/js/components/quizzes/Quiz.vue
@@ -1,11 +1,22 @@
 <script setup lang="ts">
 import QuizPicture from '@/components/quizzes/ui/QuizPicture.vue';
 import Badge from '@/components/ui/badge/Badge.vue';
+import { formatDate } from '@/lib/utils';
 import type { IQuiz } from '@/types';
-import { defineProps } from 'vue';
+import { computed, defineProps } from 'vue';
 import Actions from './ui/Actions.vue';
 
-defineProps<{ quiz: IQuiz }>();
+const props = defineProps<{ quiz: IQuiz }>();
+
+const activeClass = computed(() =>
+    props.quiz.status
+        ? 'bg-chart-2 text-destructive-foreground'
+        : 'bg-destructive text-destructive-foreground',
+);
+
+const activeStatus = computed(() =>
+    props.quiz.status ? 'Active' : 'Inactive',
+);
 </script>
 <template>
     <div>
@@ -31,17 +42,11 @@ defineProps<{ quiz: IQuiz }>();
                     </div>
                 </div>
                 <div class="py-2">
-                    <Badge
-                        :class="
-                            quiz.status
-                                ? 'bg-chart-2 text-destructive-foreground'
-                                : 'bg-destructive text-destructive-foreground'
-                        "
-                    >
-                        {{ quiz.status ? 'Active' : 'Inactive' }}
+                    <Badge :class="activeClass">
+                        {{ activeStatus }}
                     </Badge>
                     <p class="py-2 text-sm text-gray-600">
-                        expires on : {{ quiz.expire_date?.split(' ')[0] }}
+                        expires on : {{ formatDate(quiz.expire_date) }}
                     </p>
                 </div>
                 <p class="mb-4 line-clamp-3 text-sm text-gray-600">
--- resources/js/lib/utils.ts
+++ resources/js/lib/utils.ts
@@ -16,3 +16,7 @@ export function urlIsActive(
 export function toUrl(href: NonNullable<InertiaLinkProps['href']>) {
     return typeof href === 'string' ? href : href?.url;
 }
+
+export function formatDate(dateString: string | null): string {
+    return dateString?.split(' ')[0] || '';
+}

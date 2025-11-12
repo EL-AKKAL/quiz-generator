2180d57bf4b23b754d99ac11e729eaaa3ad06688
EL AKKAL AYOUB <akkalayoub3@gmail.com>
2025-11-12 15:51:21 +0000

    chore : remove unused code from guestLayout

--- resources/js/layouts/GuestLayout.vue
+++ resources/js/layouts/GuestLayout.vue
@@ -6,7 +6,7 @@ import AppLogo from '@/components/AppLogo.vue';
     <div class="container mx-auto flex min-h-screen flex-col md:max-w-2/3">
         <div class="flex w-full items-start justify-between py-4">
             <div class="flex w-fit flex-col items-center justify-center">
-                <AppLogo href="#" class="h-10 !w-40" />
+                <AppLogo />
             </div>
         </div>
         <slot />

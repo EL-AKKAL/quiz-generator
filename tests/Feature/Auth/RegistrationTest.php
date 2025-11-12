6cb511f081323f57295d05fc02bd9630cf183718
EL AKKAL AYOUB <akkalayoub3@gmail.com>
2025-11-12 01:00:33 +0000

    refactor: update registration tests to use Pest syntax and organize test structure

--- tests/Feature/Auth/RegistrationTest.php
+++ tests/Feature/Auth/RegistrationTest.php
@@ -1,19 +1,22 @@
 <?php
 
-test('registration screen can be rendered', function () {
-    $response = $this->get(route('register'));
+use function Pest\Laravel\{get, post, assertAuthenticated};
 
-    $response->assertStatus(200);
-});
+describe("Registration", function () {
 
-test('new users can register', function () {
-    $response = $this->post(route('register.store'), [
-        'name' => 'Test User',
-        'email' => 'test@example.com',
-        'password' => 'password',
-        'password_confirmation' => 'password',
-    ]);
+    it('shows the register screen', function () {
+        get(route('register'))->assertOk();
+    });
 
-    $this->assertAuthenticated();
-    $response->assertRedirect(route('dashboard', absolute: false));
+    it('registers a new user successfully', function () {
+
+        post(route('register.store'), [
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+            'password' => 'password',
+            'password_confirmation' => 'password',
+        ])->assertRedirect(route('dashboard', absolute: false));
+
+        assertAuthenticated();
+    });
 });

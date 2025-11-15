549250338328f39842fd6b8a7d089fcd50b5b881
EL AKKAL AYOUB <akkalayoub3@gmail.com>
2025-11-15 00:16:38 +0000

    Fix error : window title is undefined on edit / create quiz

--- resources/js/pages/Quizzes/Edit.vue
+++ resources/js/pages/Quizzes/Edit.vue
@@ -3,23 +3,25 @@ import QuizController from '@/actions/App/Http/Controllers/QuizController';
 import Form from '@/components/quizzes/Form.vue';
 import AppLayout from '@/layouts/AppLayout.vue';
 import { IQuiz, type BreadcrumbItem } from '@/types';
-import { usePage } from '@inertiajs/vue3';
+import { Head, usePage } from '@inertiajs/vue3';
 const page = usePage();
 
-const id = page.props.quiz ? (page.props.quiz as IQuiz).id : undefined;
-const title = page.props.quiz ? (page.props.quiz as IQuiz).title : '';
+const quiz = page.props.quiz ? (page.props.quiz as IQuiz) : undefined;
+
+const title = quiz?.id ? `Edit : ${quiz.title}` : 'New Quiz';
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
-        title: id ? `Edit ${title}` : 'New Quiz',
-        href: id
-            ? QuizController.edit.url({ quiz: id })
+        title: title,
+        href: quiz?.id
+            ? QuizController.edit.url({ quiz: quiz.id })
             : QuizController.create.url(),
     },
 ];
 </script>
 <template>
+    <Head :title="title" />
     <AppLayout :breadcrumbs="breadcrumbs">
-        <Form :key="id" />
+        <Form :key="quiz?.id" />
     </AppLayout>
 </template>

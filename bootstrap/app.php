628fc08e00c37c68d10cd5cf166e84af16696a3f
EL AKKAL AYOUB <akkalayoub3@gmail.com>
2026-03-02 14:56:36 +0000

    remove unused parameter $exception

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,7 +3,6 @@
 use App\Http\Middleware\HandleAppearance;
 use App\Http\Middleware\HandleInertiaRequests;
 use Illuminate\Foundation\Application;
-use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets;
 
@@ -22,6 +21,4 @@ return Application::configure(basePath: dirname(__DIR__))
             AddLinkHeadersForPreloadedAssets::class,
         ]);
     })
-    ->withExceptions(function (Exceptions $exceptions) {
-        //
-    })->create();
+    ->withExceptions(function () { })->create();

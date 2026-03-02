66670063215b7a1473691edcb6ae4ab4d693d30c
EL AKKAL AYOUB <akkalayoub3@gmail.com>
2026-03-02 15:17:19 +0000

    update increment-version to use proper imports

--- increment-version.mjs
+++ increment-version.mjs
@@ -1,6 +1,6 @@
-import fs from "fs";
-import path from "path";
-import { fileURLToPath } from "url";
+import fs from 'node:fs';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -12,8 +12,8 @@ const incrementVersion = () => {
     const packageJson = JSON.parse(fs.readFileSync(packagePath, "utf8"));
 
     const versionParts = packageJson.version
-        .split(".")
-        .map((part) => parseInt(part, 10));
+        .split('.')
+        .map((part) => Number.parseInt(part, 10));
 
     versionParts[2] += 1;
     const newVersion = versionParts.join(".");

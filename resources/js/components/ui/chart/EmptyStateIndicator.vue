c9d6132ab4495ab829c607c6ac4cdee6ad431a3d
EL AKKAL AYOUB <akkalayoub3@gmail.com>
2026-03-22 08:22:16 +0100

    add empty state and loading state components

---
+++ resources/js/components/ui/chart/EmptyStateIndicator.vue
@@ -0,0 +1,7 @@
+<template>
+    <div
+        class="text-muted-color text-sm flex flex-col items-center justify-center border border-dashed rounded-lg dark:border-surface-700 gap-3.5 min-h-[250px] h-[calc(100%-4rem)]">
+        <span class="pi pi-info-circle text-xl" />
+        <span class="text-muted-color text-xs"> No Data . </span>
+    </div>
+</template>
---
+++ resources/js/components/ui/chart/LoadingIndicator.vue
@@ -0,0 +1,7 @@
+<template>
+    <div
+        class="text-muted-color text-sm flex items-center justify-center border border-dashed rounded-lg dark:border-surface-700 gap-2 min-h-[250px]">
+        <span class="pi pi-spin pi-spinner text-md" />
+        <span class="text-muted-color text-xs"> loading... </span>
+    </div>
+</template>
